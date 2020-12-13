c573552f78876019c4ff5708076ef0efa020e70c
Ezequiel Barbero <ezequiel.barba94@gmail.com>
2020-12-13 19:47:25 -0300

    Creates a thumbnail when uploading an image with the Avatar component

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,37 +1,63 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import styled from 'styled-components'
 import {BORDER_RADIUS} from './css/Dimensions'
+import Resizer from 'react-image-file-resizer'
 import bandit from './icons/bandit.png'
 import {SKELETON_ANIMATION_INFO} from './css/Skeleton'
 
 export default function Avatar({onChange, src, alt, width = '135px', height = '135px'}: AvatarProps) {
-  const handleOnChange = (e: any) => {
-    const file = e.target.files[0]
-    const reader = new FileReader()
-    reader.readAsDataURL(file)
-    reader.onload = () => {
-      const base64 = reader.result as string
-      onChange(base64)
-    }
-  }
+  const [imageShowed, setImageShowed] = useState<string>('')
+  useEffect(() => setImageShowed(getImage(src)), [src])
   const showSkeleton = src === undefined
+  const handleOnChange = async (e: React.ChangeEvent<HTMLInputElement>) =>
+    await changeDisplayedImage(e, setImageShowed, onChange)
   return (
     <label>
       <Input type="file" onChange={handleOnChange}/>
-      <Image src={getImage(src)} alt={alt} width={width} height={height} skeleton={showSkeleton}/>
+      <Image src={imageShowed} alt={alt} width={width} height={height} skeleton={showSkeleton}/>
     </label>
   )
 }
 
-function getImage(url: string | null | undefined): string | undefined {
+async function changeDisplayedImage(e: React.ChangeEvent<HTMLInputElement>,
+                                    setImageShowed: (value: string) => void,
+                                    onChange: (base64: string, thumbnail: string) => void) {
+  if (!e.target.files) return
+  const file = e.target.files[0]
+  const image = await createThumbnail(file)
+  setImageShowed(image.thumbnail)
+  onChange(image.original, image.thumbnail)
+}
+
+async function encode(file: File): Promise<string> {
+  const data = await file.arrayBuffer()
+  const binary = new Uint8Array(data)
+  const binaryString = binary.join('')
+  return btoa(binaryString)
+}
+
+async function createThumbnail(file: File) {
+  const originalPromise = encode(file)
+  const thumbnailPromise = resizeImage(file)
+  const [original, thumbnail] = await Promise.all([originalPromise, thumbnailPromise])
+  return {original, thumbnail}
+}
+
+function getImage(url: string | null | undefined): string {
   if (url === undefined) return 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciLz4'
   if (url === null) return bandit
   if (url.trim().length === 0) return bandit
   return url
 }
 
+const resizeImage = (file: Blob): Promise<string> => new Promise(resolve =>
+  Resizer.imageFileResizer(
+    file, 70, 70, 'JPEG', 100, 0,
+    resolve as any, 'base64', 70, 70,
+  ))
+
 export type AvatarProps = {
-  onChange: (base64: string) => void
+  onChange: (base64: string, thumbnail: string) => void
   src?: string | null
   alt: string
   width?: string
