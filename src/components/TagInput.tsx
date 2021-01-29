ddd21a46d8ee3640266abe97dc6c9116c00055ae
Raghost <zogar1993@hotmail.com>
2021-01-29 01:41:34 -0300

    Added mode to text

---
+++ src/components/TagInput.tsx
@@ -0,0 +1,36 @@
+import React from "react"
+import styled from "styled-components"
+import { BORDER_RADIUS, SEPARATION } from 'components/css/Dimensions'
+import Flex from 'components/Flex'
+
+export default function TagInput({name, values, onAdd, onDelete}: Props) {
+  //{values.map(x => <Tag><Centerer>{x}</Centerer></Tag>)}
+	return (
+		<Flex>
+			<Label>{name}:</Label>
+		</Flex>
+	)
+}
+
+type Props = {
+	name: string
+	values: Array<string>
+	onAdd?: (value: string) => void
+	onDelete?: (value: string) => void
+}
+
+const Label = styled.label`
+    height: 18px;
+    font-size: 16px;
+    color: black;
+`
+
+const Tag = styled.span`
+    font-size: 12px;
+    box-sizing: border-box;
+    height: 16px;
+    border: 1px solid lightgray;
+    border-radius: ${BORDER_RADIUS};
+    padding: 3px 5px 3px 5px;
+    margin-left: ${SEPARATION};
+`
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -5,26 +5,35 @@ import Span from 'components/inner_components/Span'
 import styled from 'styled-components'
 import { BORDER_RADIUS } from 'components/css/Dimensions'
 
-export default function Text({children, tooltip, ...props}: Props) {
+export default function Text({children, tooltip, mode, ...props}: TextProps) {
     return (
         <Container {...props} skeleton={children === undefined}>
-            <Span {...props} title={tooltip}>{children}</Span>
+            <Span {...props} color={mode ? color(mode) : props.color} title={tooltip}>{children}</Span>
         </Container>
     )
+}
+
+function color(mode: TextMode): string {
+  if (mode === "positive") return "green"
+  if (mode === "negative") return "red"
+  return "black"
 }
 
 const Container = styled(Flex)`
   border-radius: ${BORDER_RADIUS};
 `
 
-interface Props extends DimensionsProps {
+export type TextProps = {
   children: ReactNode
   font?: string
   size?: "small" | "medium" | "large"
   bold?: boolean
-  color?: string
+  color?: string//TODO Should not let set color
   "x-align"?: XAlignType
   "y-align"?: YAlignType
   bordered?: boolean
   tooltip?: string
-}
+  mode?: TextMode
+} & DimensionsProps
+
+type TextMode =  "default" | "positive" | "negative"
