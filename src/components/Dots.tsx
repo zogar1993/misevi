f134f811946ba58cb267a8b37ca474ad9ed5bc4e
Raghost <zogar1993@hotmail.com>
2021-02-26 21:51:27 -0300

    Refactor.

--- src/components/Dots.tsx
+++ src/components/Dots.tsx
@@ -43,29 +43,13 @@ export default function Dots({ total, value, onChange, reversed, rows, advances 
       height={height}
       no-pointer-events={value === undefined}
     >
-      <Div
-        width="14px"
-        height="14px"
-        left={reversed ? undefined : '-14px'}
-        right={reversed ? '-14px' : undefined}
-        bottom={reversed ? '0px' : undefined}
-        position="absolute"
-      >
-        <IconX tentative={tentative} />
-        <DotZero
-          value={0}
-          checked={0 === value}
-          type="radio"
-          onChange={onChange && ((e: any) => {
-            onChange(Number(e.target.value))
-            setTentative(null)
-          })}
-          disabled={onChange === undefined}
-          onMouseEnter={() => setTentative(0)}
-          onMouseLeave={() => setTentative(null)}
-          visible={tentative !== null}
-        />
-      </Div>
+      <DotZero
+        value={value}
+        tentative={tentative}
+        setTentative={setTentative}
+        onChange={onChange}
+        reversed={reversed}
+      />
       <Flex
         position="relative"
         width="100%"
@@ -101,7 +85,7 @@ export interface DotsProps {
   advances?: number
 }
 
-const DotZero = styled(NoStyleInput)<{visible: boolean}>`
+const DotZeroElement = styled(NoStyleInput)<{visible: boolean}>`
   width: 14px;
   height: 14px;
   border: 1px ${({ visible }) => visible ? "solid" : "hidden"} black;
@@ -114,25 +98,64 @@ const DotZero = styled(NoStyleInput)<{visible: boolean}>`
   }
 `
 
+const Svg = styled.svg.attrs(_ => ({
+  height: "6px",
+  width: "6px",
+  viewBox: "0 0 329.26933 329",
+  xmlns: "http://www.w3.org/2000/svg"
+}))<{visible: boolean}>`
+  ${({visible}) => visible ? "" : "visibility: hidden;"};
+  pointer-events: none;
+  position: absolute;
+`
+
 function IconX({ tentative }: { tentative: number | null }) {
   return (
+    <Svg visible={tentative !== null}>
+      <g fill={tentative === 0 ? 'darkred' : 'black'}>
+        <path
+          d="m21.339844 329.398438c-5.460938 0-10.925782-2.089844-15.082032-6.25-8.34375-8.339844-8.34375-21.824219 0-30.164063l286.589844-286.59375c8.339844-8.339844 21.824219-8.339844 30.164063 0 8.34375 8.339844 8.34375 21.824219 0 30.164063l-286.589844 286.59375c-4.183594 4.179687-9.621094 6.25-15.082031 6.25zm0 0" />
+        <path
+          d="m307.929688 329.398438c-5.460938 0-10.921876-2.089844-15.082032-6.25l-286.589844-286.59375c-8.34375-8.339844-8.34375-21.824219 0-30.164063 8.339844-8.339844 21.820313-8.339844 30.164063 0l286.589844 286.59375c8.34375 8.339844 8.34375 21.824219 0 30.164063-4.160157 4.179687-9.621094 6.25-15.082031 6.25zm0 0" />
+      </g>
+    </Svg>
+  )
+}
+
+type DotZeroProps = {
+  reversed: boolean | undefined
+  tentative: number | null
+  value: number | undefined
+  setTentative: (value: number | null) => void
+  onChange: undefined | ((value: number) => void)
+}
+
+function DotZero({reversed, tentative, setTentative, onChange, value}: DotZeroProps) {
+  return (
     <Flex
-      visible={tentative !== null}
-      no-pointer-events
       width="14px"
       height="14px"
+      left={reversed ? undefined : '-14px'}
+      right={reversed ? '-14px' : undefined}
+      bottom={reversed ? '0px' : undefined}
       position="absolute"
       x-align="center"
       y-align="center"
     >
-      <svg height="6px" viewBox="0 0 329.26933 329" width="6px" xmlns="http://www.w3.org/2000/svg">
-        <g fill={tentative === 0 ? 'darkred' : 'black'}>
-          <path
-            d="m21.339844 329.398438c-5.460938 0-10.925782-2.089844-15.082032-6.25-8.34375-8.339844-8.34375-21.824219 0-30.164063l286.589844-286.59375c8.339844-8.339844 21.824219-8.339844 30.164063 0 8.34375 8.339844 8.34375 21.824219 0 30.164063l-286.589844 286.59375c-4.183594 4.179687-9.621094 6.25-15.082031 6.25zm0 0" />
-          <path
-            d="m307.929688 329.398438c-5.460938 0-10.921876-2.089844-15.082032-6.25l-286.589844-286.59375c-8.34375-8.339844-8.34375-21.824219 0-30.164063 8.339844-8.339844 21.820313-8.339844 30.164063 0l286.589844 286.59375c8.34375 8.339844 8.34375 21.824219 0 30.164063-4.160157 4.179687-9.621094 6.25-15.082031 6.25zm0 0" />
-        </g>
-      </svg>
+      <IconX tentative={tentative} />
+      <DotZeroElement
+        value={0}
+        checked={0 === value}
+        type="radio"
+        onChange={onChange && ((e: any) => {
+          onChange(Number(e.target.value))
+          setTentative(null)
+        })}
+        disabled={onChange === undefined}
+        onMouseEnter={() => setTentative(0)}
+        onMouseLeave={() => setTentative(null)}
+        visible={tentative !== null}
+      />
     </Flex>
   )
 }
