b4bbb0eae63c4425375b2ce9010f17f0017d0e70
Raghost <zogar1993@hotmail.com>
2021-02-28 01:23:45 -0300

    v0.3.8

---
+++ src/stories/items_modal/ItemsModalTemplate.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Field, {FieldProps, FieldTextProps} from "components/Field"
+
+export function Template(args: FieldProps) {
+  return <Field {...args}/>
+}
+
+export function TextFieldStoryTypes(props: FieldTextProps) {
+
+}
+
