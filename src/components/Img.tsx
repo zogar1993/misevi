969e9e1a1b731123529fdb4e887d7e69c389bdae
Raghost <zogar1993@hotmail.com>
2021-04-13 00:26:22 -0300

    feat: bordered image feat: floating image

--- src/components/Img.tsx
+++ src/components/Img.tsx
@@ -7,20 +7,19 @@ const Img = styled.img<ImageProps>`
   cursor: ${props => props.onClick ? 'pointer' : props.disabled ? 'not-allowed' : 'default'};
   visibility: ${props => props.hide ? 'hidden' : 'visible'};
   filter: ${props => props.disabled ? 'invert(20%)' : null};
-  border-radius: ${({
-                      'rounded-borders': roundedBorders,
-                      circle
-                    }) => circle ? '50%' : roundedBorders ? BORDER_RADIUS : '0'};
+  border-radius: ${({circle}) => circle ? '50%' : BORDER_RADIUS };
+  ${({float}) => float ? `float: ${float}` : '' };
+  ${({bordered}) => bordered ? `border: 1px lightgray solid` : '' };
 
   ${dimensions};
   ${positions};
 `
 export default Img
 
-export interface ImageProps extends DimensionsProps,
-  PositionsProps {
+export type ImageProps = {
   hide?: boolean
   disabled?: boolean
-  'rounded-borders'?: boolean
   circle?: boolean
-}
+  float?: string
+  bordered?: boolean
+} & DimensionsProps & PositionsProps
