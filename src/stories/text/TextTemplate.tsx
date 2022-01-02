6a2ab700e845c2c3c7a638df34c8ac20f2bf4efa
Facundo Garcia <facundogarcia1993@gmail.com>
2022-01-02 15:22:25 -0300

    v0.4.2

--- src/stories/text/TextTemplate.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import Text, { TextProps } from 'components/Text'
-
-export default function Template({ ...args }: TextProps) {
-  return <Text {...args}>{args.children}</Text>
-}
