d80194bd33bb1a8802b0656d69c7221d9aca0842
Facundo Garcia <facundogarcia1993@gmail.com>
2022-02-28 17:23:31 -0300

    add custom resize sizes to avatar

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,86 +1,101 @@
-import React from 'react'
+import React, { ChangeEvent } from 'react'
 import styled from 'styled-components'
 import { BORDER_RADIUS } from './css/Dimensions'
 import Resizer from 'react-image-file-resizer'
 import bandit from './icons/bandit.png'
+import transparent from './icons/transparent-pixel.png'
 import { SKELETON_ANIMATION_CSS } from './css/Skeleton'
 
-export default function Avatar({ onChange, src, alt, area, width = '143px', height = '143px' }: AvatarProps) {
+export default function Avatar({
+  onChange,
+  src,
+  alt,
+  area,
+  width = '143px',
+  height = '143px',
+  resizes = []
+}: AvatarProps) {
   const showSkeleton = src === undefined
-  const handleOnChange = async (e: React.ChangeEvent<HTMLInputElement>) =>
-    await changeDisplayedImage(e, onChange)
+  const handleOnChange = async (e: ChangeEvent<HTMLInputElement>) =>
+    await changeDisplayedImage(e, onChange, resizes)
   return (
     <Label width={width} height={height} area={area}>
-      <Input type="file" onChange={handleOnChange} />
-      <Image src={getImage(src)} alt={alt} width={width} height={height} skeleton={showSkeleton} />
+      <Input type='file' onChange={handleOnChange} />
+      <>
+        <Image src={getImage(src)} alt={alt} width={width} height={height} skeleton={showSkeleton} />
+      </>
     </Label>
   )
 }
 
-async function changeDisplayedImage(e: React.ChangeEvent<HTMLInputElement>,
-                                    onChange: (base64: string, thumbnail: string) => void) {
+async function changeDisplayedImage(
+  e: ChangeEvent<HTMLInputElement>,
+  onChange: Required<AvatarProps>['onChange'],
+  resizes: Required<AvatarProps>['resizes']
+) {
   if (!e.target.files) return
   const file = e.target.files[0]
-  const image = await createThumbnail(file)
-  onChange(image.original, image.thumbnail)
+  const image = await createResizedImage(file, resizes)
+  onChange(image.original, image.resizes)
 }
 
 async function encode(file: File): Promise<string> {
   const promise = new Promise<string>((resolve, reject) => {
     const reader = new FileReader()
-    reader.onload = ev => resolve(ev.target!.result as string)
+    reader.onload = (ev) => resolve(ev.target!.result as string)
     reader.onerror = reject
     reader.readAsDataURL(file)
   })
   return await promise
 }
 
-async function createThumbnail(file: File) {
-  const originalPromise = encode(file)
-  const thumbnailPromise = resizeImage(file)
-  const [original, thumbnail] = await Promise.all([originalPromise, thumbnailPromise])
-  return { original, thumbnail }
+async function createResizedImage(file: File, resizes: Required<AvatarProps>['resizes']) {
+  const result = await Promise.all([
+    encode(file),
+    ...resizes.map((resize) => resizeImage(file, resize))
+  ])
+  return { original: result[0], resizes: result.splice(1) }
 }
 
 function getImage(url: string | null | undefined): string {
-  if (url === undefined) return 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciLz4'
+  if (url === undefined) return transparent
   if (url === null) return bandit
   if (url.trim().length === 0) return bandit
   return url
 }
 
-const resizeImage = (file: Blob): Promise<string> => new Promise(resolve =>
-  Resizer.imageFileResizer(
-    file, 70, 70, 'JPEG', 100, 0,
-    resolve as any, 'base64', 70, 70
-  ))
+const resizeImage = (file: Blob, resize: number): Promise<string> =>
+  new Promise((resolve) =>
+    Resizer.imageFileResizer(file, resize, resize, 'JPEG', 100, 0, resolve as any, 'base64', 70, 70)
+  )
 
 export type AvatarProps = {
-  onChange: (base64: string, thumbnail: string) => void
+  onChange: (base64: string, customs: Array<string>) => void
   src?: string | null
   alt: string
   width?: string
   height?: string
   area?: string
+  resizes?: Array<number>
 }
 
-const Image = styled.img<{ width?: string, height?: string, skeleton: boolean }>`
+const Image = styled.img<{ width?: string; height?: string; skeleton: boolean }>`
   border-radius: ${BORDER_RADIUS};
   width: ${({ width }) => width};
   height: ${({ height }) => height};
   object-fit: scale-down;
   cursor: pointer;
   border: none;
-  ${({ skeleton }) => skeleton ? SKELETON_ANIMATION_CSS : ''}
+  ${({ skeleton }) => (skeleton ? SKELETON_ANIMATION_CSS : '')}
 `
 
 const Input = styled.input`
   display: none;
 `
 
-const Label = styled.label<{ width?: string, height?: string, area?: string }>`
+const Label = styled.label<{ width?: string; height?: string; area?: string }>`
   all: unset;
   width: ${({ width }) => width};
   height: ${({ height }) => height};
-  ${({area}) => area ? `grid-area: ${area}` : ''};
+  ${({ area }) => (area ? `grid-area: ${area}` : '')};
 `
--- src/stories/avatar/AvatarTemplate.tsx
+++ src/stories/avatar/AvatarTemplate.tsx
@@ -1,17 +1,54 @@
-import Avatar, {AvatarProps} from '../../components/Avatar'
-import React, { useState } from "react";
+import styled from 'styled-components'
+import Avatar, { AvatarProps } from '../../components/Avatar'
+import React, { useState } from 'react'
 
-export default function ({...args}: AvatarProps) {
+export default function ({ ...args }: AvatarProps) {
   const [image, setImage] = useState<string>()
-  const [thumbnail, setThumbnail] = useState<string>()
+  const [resizes, setResizes] = useState<Array<string>>([])
   return (
     <div>
-      <Avatar {...args} onChange={(image, thumbnail) => {
-        setImage(image)
-        setThumbnail(thumbnail)
-      }}/>
-      <img src={image}/>
-      <img src={thumbnail}/>
+      <Avatar
+        {...args}
+        onChange={(image, thumbnail) => {
+          setImage(image)
+          setResizes(thumbnail)
+        }}
+        resizes={RESIZES.map(x => x*2)}
+      />
+      {resizes.map((resize, i) => {
+        const size = RESIZES[i]
+        return (
+          <Container key={`${size}`}>
+            {size}x{size}
+            <HorizontalContainer>
+              <Image src={resize} alt={`${size}`} size={size} />
+              <Image src={image} alt={`original at ${size}`} size={size} />
+            </HorizontalContainer>
+          </Container>
+        )
+      })}
+      {image && (
+        <Container>
+          Original
+          <img src={image} alt='original' />
+        </Container>
+      )}
     </div>
   )
 }
+
+const RESIZES = [70, 143]
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+const HorizontalContainer = styled.div`
+  display: flex;
+  gap: 8px;
+`
+
+const Image = styled.img<{ size: number }>`
+  width: ${(x) => x.size}px;
+  height: ${(x) => x.size}px;
+`
