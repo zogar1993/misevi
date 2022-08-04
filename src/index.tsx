f79af540bbc94046c60d0af50a7cde49583bb55b
Facundo Garcia <facundogarcia-ae@team.asana.com>
2022-08-04 01:24:16 -0300

    add CheckComboBox to components

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
-import './styles/Fonts.css'
-export { default as CircularNumberInput } from './components/CircularNumberInput'
-export { default as Dots } from './components/Dots'
-export { default as Avatar } from './components/Avatar'
-export { default as Field } from './components/Field'
-export { default as RadioButton } from './components/RadioButton'
-export { default as CheckButton } from './components/CheckButton'
-export { SKELETON_ANIMATION_CSS } from './components/css/Skeleton'
-export { default as theme } from './components/theme/Theme'
+import "./styles/Fonts.css"
+
+export {default as CircularNumberInput} from "./components/CircularNumberInput"
+export {default as Dots} from "./components/Dots"
+export {default as Avatar} from "./components/Avatar"
+export {default as Field} from "./components/Field"
+export {default as RadioButton} from "./components/RadioButton"
+export {default as CheckButton} from "./components/CheckButton"
+export {default as CheckComboBox} from "./components/CheckComboBox"
+export {SKELETON_ANIMATION_CSS} from "./components/css/Skeleton"
+export {default as theme} from "./components/theme/Theme"
